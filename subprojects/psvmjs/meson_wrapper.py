660ea2e925fa5f605ce50076d5cfbf3efbe5b7d5
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-02-12 14:21:32 -0800

    Update meson_wrapper.py
    Block when running subprocesses

--- subprojects/psvmjs/meson_wrapper.py
+++ subprojects/psvmjs/meson_wrapper.py
@@ -11,11 +11,11 @@ def main():
     qjsc_executable = Path(sys.argv[3])
 
     # Bundle the JS project
-    subprocess.Popen([npm_path, "i"], cwd = psvmjs_src_dir)
-    subprocess.Popen([npm_path, "run", "build"], cwd = psvmjs_src_dir)
+    subprocess.Popen([npm_path, "i"], cwd = psvmjs_src_dir).wait()
+    subprocess.Popen([npm_path, "run", "build"], cwd = psvmjs_src_dir).wait()
 
     # Compile to bytecode
-    subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir)
+    subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir).wait()
 
 
 if __name__ == "__main__":
