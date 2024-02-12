0d46a85daad783d8f721bae32a44e41e88caf2c0
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-02-12 14:14:16 -0800

    Fix file paths when subproject

--- subprojects/psvmjs/meson_wrapper.py
+++ subprojects/psvmjs/meson_wrapper.py
@@ -1,18 +1,22 @@
 import shutil
 import subprocess
 from pathlib import Path
+import sys
 
-npm_path = shutil.which("npm")
-build_dir = Path.cwd()
+def main():
+    npm_path = shutil.which("npm")
+    psvmjs_src_dir = (Path(sys.argv[1]) / "js_src").resolve()
+    psvmjs_build_dir = Path(sys.argv[2])
+    
+    qjsc_executable = Path(sys.argv[3])
 
-psvmjs_src_dir = (build_dir / "../subprojects/psvmjs/js_src").resolve()
-psvmjs_build_dir = (build_dir / "subprojects/psvmjs").resolve()
+    # Bundle the JS project
+    subprocess.Popen([npm_path, "i"], cwd = psvmjs_src_dir)
+    subprocess.Popen([npm_path, "run", "build"], cwd = psvmjs_src_dir)
 
-qjsc_executable = (build_dir / "subprojects/quickjs/qjsc").resolve()
+    # Compile to bytecode
+    subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir)
 
-# Bundle the JS project
-subprocess.Popen([npm_path, "i"], cwd = psvmjs_src_dir)
-subprocess.Popen([npm_path, "run", "build"], cwd = psvmjs_src_dir)
 
-# Compile to bytecode
-subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir)
+if __name__ == "__main__":
+    main()
