2f63a19534345c9c154df16ee28991b27504b421
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-02-15 20:03:57 -0800

    separate qjsc task from npm build task

--- subprojects/psvmjs/meson_wrapper.py
+++ subprojects/psvmjs/meson_wrapper.py
@@ -6,16 +6,13 @@ import sys
 def main():
     npm_path = shutil.which("npm")
     psvmjs_src_dir = (Path(sys.argv[1]) / "js_src").resolve()
-    psvmjs_build_dir = Path(sys.argv[2])
-    
-    qjsc_executable = Path(sys.argv[3])
 
     # Bundle the JS project
     subprocess.Popen([npm_path, "i"], cwd = psvmjs_src_dir).wait()
     subprocess.Popen([npm_path, "run", "build"], cwd = psvmjs_src_dir).wait()
 
     # Compile to bytecode
-    subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir).wait()
+    # subprocess.Popen([qjsc_executable, "-c", "-o", (psvmjs_build_dir / "psvmjs.c").resolve(), "dist/globalize.js"], cwd = psvmjs_src_dir).wait()
 
 
 if __name__ == "__main__":
